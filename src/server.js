a46fb967e317bff572f94f46079099cb5176f6b3
Junior Cueva <junior.cueva@tecsup.edu.pe>
2026-09-23 11:10:21 -0500

    Estructura inicial del proyecto SecureDocs

---
+++ src/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/api/salud', (req, res) => {
+  res.json({ mensaje: 'SecureDocs funcionando' });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor en http://localhost:${PORT}`);
+});
